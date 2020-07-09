240e5e5c46be27d052b1670eb92e3239ce650ff5
DarraghBurkeMS <67023889+DarraghBurkeMS@users.noreply.github.com>
2020-07-09 13:27:22 -0700

    Update Floodfill.js

--- Floodfill.js
+++ Floodfill.js
@@ -12,7 +12,7 @@ tool = {
       this.keyboardObservable = this.scene.onKeyboardObservable.add((kbInfo) => {
         if (kbInfo.event.key === 'f') {
           const ctx = this.canvas2D.getContext('2d');
-          ctx.fillStyle = 'pink';
+          ctx.fillStyle = Math.floor(Math.random()*16777215).toString(16);
           ctx.fillRect(0,0, this.size.width, this.size.height);
           this.update();
         }
