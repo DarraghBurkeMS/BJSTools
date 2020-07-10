8ad2bb6ede7caea50ca08e000ddc46506ab2c74c
DarraghBurkeMS <67023889+DarraghBurkeMS@users.noreply.github.com>
2020-07-10 13:13:24 -0700

    Update Floodfill.js

--- Floodfill.js
+++ Floodfill.js
@@ -9,8 +9,8 @@ tool = {
       this.update = update;
     }
     setup () {
-      this.keyboardObservable = this.scene.onKeyboardObservable.add((kbInfo) => {
-        if (kbInfo.event.key === 'f') {
+      this.pointerObservable = this.scene.onPointerObservable.add((pointerInfo) => {
+        if (pointerInfo.pickInfo.hit && pointerInfo.type == 0x01) {
           const ctx = this.canvas2D.getContext('2d');
           ctx.fillStyle = '#' + Math.floor(Math.random()*16777215).toString(16);
           ctx.fillRect(0,0, this.size.width, this.size.height);
@@ -19,8 +19,8 @@ tool = {
       });
     }
     cleanup () {
-      if (this.keyboardObservable) {
-        this.scene.onKeyboardObservable.remove(this.keyboardObservable);
+      if (this.pointerObservable) {
+        this.scene.onPointerObservable.remove(this.pointerObservable);
       }
     }
   }
