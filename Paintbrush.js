9132b47810797abaed9e288df966e8e4e56397c9
DarraghBurkeMS <67023889+DarraghBurkeMS@users.noreply.github.com>
2020-07-10 19:06:53 -0700

    Update Paintbrush.js

--- Paintbrush.js
+++ Paintbrush.js
@@ -2,11 +2,13 @@ let tool;
 tool = {
   name: "Paintbrush",
   type: class {
-    constructor(scene, canvas2D, size, update) {
+    constructor(scene, canvas2D, size, update, getMetadata, setMetadata) {
       this.scene = scene;
       this.canvas2D = canvas2D;
       this.size = size;
       this.update = update;
+      this.getMetadata = getMetadata;
+      this.setMetadata = setMetadata;
     }
     setup () {
       this.isPainting = false;
@@ -15,7 +17,7 @@ tool = {
           case 0x01: // MOUSEDOWN
             if (pointerInfo.event.button == 0) {
               this.isPainting = true;
-              this.paintColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+              // this.paintColor = '#' + Math.floor(Math.random()*16777215).toString(16);
             }
             break;
           case 0x02: // MOUSEUP
@@ -29,7 +31,8 @@ tool = {
                 const ctx = this.canvas2D.getContext('2d');
                 const x = pointerInfo.pickInfo.getTextureCoordinates().x * this.size.width;
                 const y = (1 - pointerInfo.pickInfo.getTextureCoordinates().y) * this.size.height;
-                ctx.fillStyle = this.paintColor;
+                ctx.fillStyle = this.getMetadata().color;
+                ctx.globalAlpha = this.getMetadata().opacity;
                 ctx.beginPath();
                 ctx.ellipse(x, y, 15, 15, 0, 0, Math.PI * 2);
                 ctx.fill();
