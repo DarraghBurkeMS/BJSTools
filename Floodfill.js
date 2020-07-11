92c7fe7511d41c68e077b8b614239c26bb7fd0af
DarraghBurkeMS <67023889+DarraghBurkeMS@users.noreply.github.com>
2020-07-10 19:07:46 -0700

    Update Floodfill.js

--- Floodfill.js
+++ Floodfill.js
@@ -2,17 +2,21 @@ let tool;
 tool = {
   name: "Floodfill",
   type: class {
-    constructor(scene, canvas2D, size, update) {
+    constructor(scene, canvas2D, size, update, getMetadata, setMetadata) {
       this.scene = scene;
       this.canvas2D = canvas2D;
       this.size = size;
       this.update = update;
+      this.getMetadata = getMetadata;
+      this.setMetadata = setMetadata;
     }
     setup () {
       this.pointerObservable = this.scene.onPointerObservable.add((pointerInfo) => {
         if (pointerInfo.pickInfo.hit && pointerInfo.type == 0x01) {
           const ctx = this.canvas2D.getContext('2d');
-          ctx.fillStyle = '#' + Math.floor(Math.random()*16777215).toString(16);
+          ctx.fillStyle = this.getMetadata().color;
+          ctx.globalAlpha = this.getMetadata().opacity;
+          //ctx.fillStyle = '#' + Math.floor(Math.random()*16777215).toString(16);
           ctx.fillRect(0,0, this.size.width, this.size.height);
           this.update();
         }
